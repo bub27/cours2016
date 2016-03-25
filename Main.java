70619d9a7b0aab400ea4c2dc76c754b202a35ccb
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-03-25 11:51:28 +0100

    Ajout variable s

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main  {
 	private int x;
 	private int y;
 	private int z;
+	private String s;
 	public static void main( String[] args ) {
 		System.out.println("Hello World");
 		System.out.println("Bye bye");
